3620196270336bd3c7de6c02d40fefa06dfe703e
HafewaBargaoui <hafewa.bargaoui@gmail.com>
2024-01-02 13:25:16 +0100

    feat: :sparkles: add yugenContext

---
+++ yugen_dev/Data/YugenContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using yugen_dev.Models;
+
+namespace yugen_dev.Data
+{
+    public class YugenContext : DbContext
+    {
+        public YugenContext(DbContextOptions<YugenContext> options) :
+       base(options)
+        {
+        }
+        public DbSet<Dish> Dishes { get; set; }
+
+    }
+
+}
