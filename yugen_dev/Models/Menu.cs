4121f3c3bf0c1dc03667e6203f5ae37d36bed314
HafewaBargaoui <hafewa.bargaoui@gmail.com>
2024-01-08 16:07:49 +0100

    fix bug git

--- yugen_dev/Models/Menu.cs
+++ yugen_dev/Models/Menu.cs
@@ -1,17 +1,11 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace yugen_dev.Models;
 
 public class Menu
 {
     public int Id { get; set; }
-
-    [Display(Name = "Nom du menu en français")]
     public string NameFr { get; set; }
-    [Display(Name = "Nom du menu en japonais")]
     public string NameJap { get; set; }
-    [Display(Name = "Prix")]
     public int Price { get; set; } //One review belong to a single client  
-    public ICollection<MenuDish>? MenusDishes { get; set; }
+    public ICollection<MenuDish> MenusDishes { get; set; }
 
 }
