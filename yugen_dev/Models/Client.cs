2648123ad599a3381501b3a319d957b74ab3f66e
HafewaBargaoui <hafewa.bargaoui@gmail.com>
2024-01-08 18:37:20 +0100

    feat: :sparkles: add French translations for panel admin  client

--- yugen_dev/Models/Client.cs
+++ yugen_dev/Models/Client.cs
@@ -1,14 +1,22 @@
+using System.ComponentModel.DataAnnotations;
 namespace yugen_dev.Models;
 
- public class Client {
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public string Address { get; set; }
-    public string PhoneNumber { get; set; }
-    public string Password { get; set; }
-    public string Preferences { get; set; }
-    public ICollection<Review> Reviews { get; set; } // A client can have several reviews
-    public ICollection<Reservation> Reservations { get; set; } // A client can have several reservations
- }
+public class Client
+{
+   public int Id { get; set; }
+   [Display(Name = "Prénom")]
+   public string FirstName { get; set; }
+   [Display(Name = "Nom")]
+   public string LastName { get; set; }
+   public string Email { get; set; }
+   [Display(Name = "Adresse")]
+   public string Address { get; set; }
+   [Display(Name = "Numéro de téléphone")]
+   public string PhoneNumber { get; set; }
+   [Display(Name = "Mot de passe")]
+   public string Password { get; set; }
+   [Display(Name = "Préférences")]
+   public string Preferences { get; set; }
+   public ICollection<Review>? Reviews { get; set; } // A client can have several reviews
+   public ICollection<Reservation>? Reservations { get; set; } // A client can have several reservations
+}
