e0ecd898093a9279cc1e25be8191abe5e0e93385
HafewaBargaoui <hafewa.bargaoui@gmail.com>
2024-01-08 19:07:11 +0100

    feat: :sparkles: add French translations for panel admin menu

--- yugen_dev/Models/Menu.cs
+++ yugen_dev/Models/Menu.cs
@@ -1,10 +1,14 @@
+using System.ComponentModel.DataAnnotations;
 namespace yugen_dev.Models;
 
 public class Menu
 {
     public int Id { get; set; }
+    [Display(Name = "Nom de menu en français")]
     public string NameFr { get; set; }
+    [Display(Name = "Nom de menu en japonais")]
     public string NameJap { get; set; }
+    [Display(Name = "Prix")]
     public int Price { get; set; } //One review belong to a single client  
     public ICollection<MenuDish> MenusDishes { get; set; }
 
