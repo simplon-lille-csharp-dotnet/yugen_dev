5820e121f7fed0a1f8b14734f7ae1bf6560d5962
Aurore Reynier <reynier.aurore@gmail.com>
2024-01-11 16:59:03 +0100

    feat: :construction: Beginning on the panel admin reservation, attention foreign key clien id non functionnal

--- yugen_dev/Models/Reservation.cs
+++ yugen_dev/Models/Reservation.cs
@@ -6,11 +6,14 @@
     public int Id { get; set; }
     [Required]
     [RegularExpression(@"^\d{1,4}$", ErrorMessage = "Invalid number of people.")]
+    [Display(Name = "Nombre de personnes")]
     public int NumberPeople { get; set; }
     [Required]
+    [Display(Name = "Date de réservation")]
     public DateTime DateReservation { get; set; }
     [Required]
     [CustomTimeReservation(ErrorMessage = "Invalid time reservation value.")]
+    [Display(Name = "Heure de réservation")]
     public TimeSpan TimeReservation { get; set; }
     public string? Message { get; set; }
     public Client? Client { get; set; } //One reservation belong to a single client  
