7bc65fa33b0b6e02c505502c2608634e0bc00b6d
Aurore Reynier <reynier.aurore@gmail.com>
2024-01-12 16:30:03 +0100

    refactor: :art: Improve display format on the panel admin

--- yugen_dev/ValidationAttributes/CustomTimeReservationAttribute.cs
+++ yugen_dev/ValidationAttributes/CustomTimeReservationAttribute.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 public class CustomTimeReservationAttribute : ValidationAttribute
 {
-    private readonly string[] AllowedValues = { "1.19:00:00", "1.19:30:00", "1.20:00:00", "1.20:30:00", "1.21:00:00", "1.21:30:00", "1.22:00:00" };
+    private readonly string[] AllowedValues = { "19:00:00", "19:30:00", "20:00:00", "20:30:00", "21:00:00", "21:30:00", "22:00:00" };
 
     public override bool IsValid(object value)
     {
